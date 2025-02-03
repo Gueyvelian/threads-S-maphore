62d5157bb44d5b56db6d641c303d8ced0db4e8ed
Your Name <you@example.com>
2025-02-03 14:57:19 +0100

    ajout du sémaphore

--- src/main/java/org/example/Consomateur.java
+++ src/main/java/org/example/Consomateur.java
@@ -1,22 +1,34 @@
 package org.example;
 
 import java.util.List;
+import java.util.concurrent.Semaphore;
 
 public class Consomateur extends monThread {
     List<String> produits;
-    public Consomateur(List<String> produits) {
+    Semaphore semaphore;
+
+    public Consomateur(List<String> produits, Semaphore s) {
         this.produits = produits;
+        this.semaphore = s;
     }
+
     @Override
     public void run() {
         while (true) {
-            if (produits.isEmpty()) break;
+            try {
+
+                semaphore.acquire(); // down du sémaphore
+            } catch (InterruptedException ex) {}
+            // début section critique
+            if (produits.isEmpty()) {semaphore.release();break;}
             int last = produits.size() - 1;
             System.out.println(getName() + "tente de lire le produit :" + last);
             String produit = produits.get(last);
             System.out.println(getName() + " a lu le produit : " + produit);
             System.out.println(getName() + " tente d'enlever le produit : " + produit);
             produits.remove(last);
+            // sortie de la section critique
+            semaphore.release(); // up du sémaphore
             System.out.println(getName() + " a enlevé le produit : " + produit);
         }
         System.out.println(getName() + " a fini");
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,16 +2,22 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        // on construit un sémaphore initialisé à 1
+        Semaphore s = new Semaphore(1);
         List<String> produits = new ArrayList<>();
         // on remplit la liste
         for (int i = 0; i < 10; i++) {
             produits.add("numéro " + i);
         }
-        Consomateur cons = new Consomateur(produits);
+        Consomateur cons = new Consomateur(produits, s);
+        Consomateur cons2 = new Consomateur(produits, s);
+
         cons.start();
+        cons2.start();
     }
 }
